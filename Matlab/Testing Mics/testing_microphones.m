48812709e1417bc2d117cb6b40a0af19182529d8
Dewan Pieterse <ptrdew001@myuct.ac.za>
2019-09-09 13:43:35 +0200

    Added Freq response files and testing scripts

--- Matlab/Testing Mics/testing_microphones.m
+++ Matlab/Testing Mics/testing_microphones.m
@@ -1,17 +1,29 @@
+clear all;clc;close all;
+
+% open('withAmp.mat');
+% open('withoutAmp.mat');
+VarName1 = xlsread('./Suspended Mic Testing/15000.xlsx');
+
+% test1 = load ('withAmp.mat');
+% test1 = test1.unnamed;
+% test2 = load ('withoutAmp.mat');
+% test2 = test2.unnamed;
+
+test1 = VarName1;
 
 fs = 44.1e3; % Sampling frequency
 ts = 1/fs; % Sampling period
-N = length(test); % Number of points 
+N = length(test1); % Number of points 
 t = (0:1:(N-1))*ts; 
 
-y = VarName1;
+y = test1;
 
 % plot our signal: time-domain
-figure;
-plot(t, y, 'r'); % t : x- axis, y: y -axis
-xlabel('Time (s)');
-ylabel('Signal y');
-grid on;
+% figure;
+% plot(t, y, 'r'); % t : x- axis, y: y -axis
+% xlabel('Time (s)');
+% ylabel('Signal y');
+% grid on;
 
 % Plot the spectrum: frequency domain
 figure; 
@@ -23,7 +35,10 @@ xlabel('Frequency (Hz)');
 ylabel('Magnitude of spectrum of y');
 title('MAX4466 Microphone - With Amp');
 
-y = test;
+% pks = findpeaks(real(20*log(fft_y)),'MinPeakDistance',25)
+
+%%
+y = test2;
 
 % plot our signal: time-domain
 figure;
