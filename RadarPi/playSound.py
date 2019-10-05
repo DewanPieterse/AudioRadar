dc8646c5a2c19ce902f5bd5eaaf8b4127c6b7d95
DewanPieterse <PTRDEW001@myuct.ac.za>
2019-10-05 13:40:45 +0200

    Added volume controls

--- RadarPi/playSound.py
+++ RadarPi/playSound.py
@@ -3,14 +3,14 @@ import pygame, os
 # from pydub import AudioSegment
 # from pydub.playback import play
 
-def playSound(fileName):
+def playSound(fileName,volume):
     
     pygame.init() # Initialize pygame 
 #     pygame.mixer.pre_init(frequency=44100, size=-16, channels=1, buffer=88200)
     file = '/home/pi/Documents/RadarPi/static/' + fileName
 #     time.sleep(1)
     sound = pygame.mixer.Sound(file) # Load the sounds
-    sound.set_volume(0.075)
+    sound.set_volume(volume)
     sound.play()
     
 #     os.system(file)
--- RadarPi/webapp.py
+++ RadarPi/webapp.py
@@ -51,10 +51,11 @@ def results():
         if mode == 'Continuous Non-Technical':
           
             duration = float(request.form["duration"])
+            volume = float(request.form["volume"])
           
             waveGenerator.waveGenerator(duration)
             name = '8000Hz.wave' 
-            playSound(name)
+            playSound(name,volume)
             Rx_Signal = recordAudio.recordAudio(duration*1.1)
             cwProcessing.cwProcessing(Rx_Signal)
 
@@ -64,10 +65,11 @@ def results():
           
             frequency = float(request.form["frequency"])
             duration = float(request.form["duration"])
+            volume = float(request.form["volume"])
 
             waveGenerator.waveGenerator(duration, frequency)
             name = str(int(frequency)) + 'Hz.wave'
-            playSound(name)
+            playSound(name,volume)
             Rx_Signal = recordAudio.recordAudio(duration*1.1)
             cwProcessing.cwProcessing(Rx_Signal,frequency)
           
@@ -76,13 +78,14 @@ def results():
         elif mode == 'Pulse Doppler Non-Technical':
           
             rangeU = float(request.form["rangeU"])
+            volume = float(request.form["volume"])
             
             Tx_Signal, Tx_p = waveGenerator.pulseTrainGenerator(rangeU)
             name = 'Chirp 8000Hz.wave'
             
             duration = 32 * ((2 * rangeU) / 343) #PRI
 #             print(duration)
-            playSound(name)
+            playSound(name,volume)
             Rx_Signal = recordAudio.recordAudio(duration*2)
             
             pdProcessing.pdProcessing(Tx_p, Rx_Signal, rangeU)
@@ -91,17 +94,18 @@ def results():
       
         elif mode == 'Pulse Doppler Technical':
           
-            rangeU = request.form["rangeU"]
-            resolution = request.form["resolution"]
-            frequency = request.form["frequency"]
-            bandwidth = request.form["bandwidth"]
-            pulses = request.form["pulses"]
+            rangeU = float(request.form["rangeU"])
+            resolution = float(request.form["resolution"])
+            frequency = float(request.form["frequency"])
+            bandwidth = float(request.form["bandwidth"])
+            pulses = float(request.form["pulses"])
+            volume = float(request.form["volume"])/100
 
             Tx_Signal, Tx_p = waveGenerator.pulseTrainGenerator(rangeU,resolution,frequency,bandwidth,pulses)
 
             name = 'Chirp ' + str(int(frequency)) + 'Hz.wave'
 
-            playSound(name)
+            playSound(name,volume)
 
             return render_template("results.html",results = results)
           
